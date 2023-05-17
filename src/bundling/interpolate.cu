819a8ad67d6342e88b94668da0816674c6478d5e
Mervin <mervin.dj@outlook.com>
2023-05-17 16:27:10 +0200

    Fixed interpolation

--- src/bundling/interpolate.cu
+++ src/bundling/interpolate.cu
@@ -13,79 +13,72 @@ bundling::interpolate(const graph& originalGraph,
 
   // *** Loop over all the edges in the source and bundled graph
   for (size_t i = 0; i < originalGraph.edges().size(); i++) {
-    const auto& sourceEdge = originalGraph.edges()[i];
-    const auto& targetEdge = bundledGraph.edges()[i];
+    const auto& originalEdge = originalGraph.edges()[i];
+    const auto& bundledEdge = bundledGraph.edges()[i];
 
-    float maxDisplacement = settings.absoluteDisplacement
-                              ? settings.displacementMax
-                              : settings.displacementMax * sourceEdge->length();
+    const float maxDisplacement =
+      settings.absoluteDisplacement
+        ? settings.displacementMax
+        : settings.displacementMax * originalEdge->length();
 
     // *** Create a vector for all the points of the edge and the displacements
-    std::vector<point_t> points(targetEdge->points().size());
-    std::vector<float> displacements(targetEdge->points().size());
+    std::vector<point_t> points(bundledEdge->points().size());
+    std::vector<float> displacements(bundledEdge->points().size());
+
+    // *** Copy the first and last points from the original target edge as they
+    // are not interpolated
+    points.front() = bundledEdge->points().front();
+    points.back() = bundledEdge->points().back();
 
     // *** Get the previous point
-    auto previousPoint = targetEdge->points().front();
+    auto previousPoint = points.front();
 
     // *** Accumulate distance from starting point to current point
     float distanceToStart = 0;
 
-    for (size_t j = 1; j < targetEdge->points().size() - 1; j++) {
+    for (size_t j = 1; j < bundledEdge->points().size() - 1; j++) {
       // *** Get the next point
-      auto currentPoint = targetEdge->at(j);
+      auto currentPoint = bundledEdge->at(j);
 
       // *** Calculate the distance between the previous and current point and
       // add it to the accumulated distance from the start
       distanceToStart += glm::distance(previousPoint, currentPoint);
 
       // *** Calculate the arc-length of the i-th point of the target edge [0,1]
-      const float t = distanceToStart / targetEdge->length();
+      const float t = distanceToStart / bundledEdge->length();
 
       // *** Calculate the corresponding index for the source (resampled target
       // line has more points)
       auto k = static_cast<size_t>(
-        t * static_cast<float>(sourceEdge->points().size() - 1));
+        t * static_cast<float>(originalEdge->points().size() - 1));
 
       // *** Calculate how far between the current and next point from the
       // source it is (assuming linear uniform sampling of the target)
-      const float u = t * static_cast<float>(sourceEdge->points().size() - 1) -
+      const float u = t * static_cast<float>(originalEdge->points().size() - 1) -
                       static_cast<float>(k);
 
       // *** Calculate the interpolated point position
       glm::vec2 p =
-        targetEdge->points()[j] * (1 - u) + targetEdge->points()[j + 1] * u;
+        originalEdge->points()[k] * (1 - u) + originalEdge->points()[k + 1] * u;
 
       // *** Store the displacement distance
       const float displacementDistance = glm::distance(currentPoint, p);
 
       // *** Effective displacement as a result of the interpolation
-      float effectiveDisplacement =
-        (1 - settings.relaxation) * displacementDistance;
+      float effectiveRelaxation = 1 - settings.relaxation;
 
       // *** Clamp the displacement
-      if (effectiveDisplacement > maxDisplacement) {
-        effectiveDisplacement = maxDisplacement / displacementDistance;
+      if (effectiveRelaxation * displacementDistance > maxDisplacement) {
+        effectiveRelaxation = maxDisplacement / displacementDistance;
       }
 
       // *** Update the current point
       points[j] =
-        currentPoint * effectiveDisplacement + p * (1 - effectiveDisplacement);
+        currentPoint * effectiveRelaxation + p * (1 - effectiveRelaxation);
 
       // *** Calculate the displacement value of the point
       displacements[j] =
-        effectiveDisplacement * displacementDistance / maxDisplacement;
-
-      // *** Separate bundles going in different directions
-      if (settings.directionalSeparation != 0) {
-        auto tan = glm::normalize(glm::vec3{ currentPoint - previousPoint, 0 });
-        auto delta = glm::cross(tan, { 0, 0, 1 });
-
-        // *** max shift should be proportional with edge length, more
-        // precisely with length / resolution
-        currentPoint += glm::vec2(delta.x, delta.y) *
-                        settings.directionalSeparation *
-                        std::pow(1.0f - 2.0f * std::abs(t - 0.5f), 0.4f);
-      }
+        effectiveRelaxation * displacementDistance / maxDisplacement;
 
       // *** Update the previous point
       previousPoint = currentPoint;
